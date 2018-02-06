c251c3d31340f423e41a4ec892eb4527d891a2cd
Jordanceldridge <36171531+Jordanceldridge@users.noreply.github.com>
2018-02-06 16:54:11 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("hi i suck");
+        System.out.println("i suck even more");
     }
 }
